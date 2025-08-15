a8670840b3371c85576470ea7be5a05156840bfd
ajaxzheng <894103554@qq.com>
2025-08-15 10:08:40 +0800

    feat: remoter 添加 unplugin 插件，加快本地启动和打包速度

--- packages/next-remoter/vite.config.ts
+++ packages/next-remoter/vite.config.ts
@@ -2,12 +2,23 @@ import { dirname, resolve } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { TinyVueSingleResolver } from '@opentiny/unplugin-tiny-vue'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [TinyVueSingleResolver]
+    }),
+    AutoImport({
+      resolvers: [TinyVueSingleResolver]
+    })
+  ],
   build: {
     sourcemap: true,
     minify: false,
--- packages/next-remoter/vite.remoter.config.ts
+++ packages/next-remoter/vite.remoter.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { TinyVueSingleResolver } from '@opentiny/unplugin-tiny-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/next-remoter/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [TinyVueSingleResolver]
+    }),
+    AutoImport({
+      resolvers: [TinyVueSingleResolver]
+    })
+  ]
 })
